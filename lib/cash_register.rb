3d44ccf2a6b7f192ce064c3830a32ac416aa2baf
Olivia Kesler <oakesler@protonmail.ch>
2019-08-14 08:04:11 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  #@@count = 0 
+  
+  
+  def initialize(total = 0.0, discount = 0)
+    @total = total
+    @discount = discount
+    #@@count += 1
+  end
+end
