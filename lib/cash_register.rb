1b76a0a8bde0503e900499d426eafabefee17b7b
Olivia Kesler <oakesler@protonmail.ch>
2019-08-15 04:46:29 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,36 +1,37 @@
 require "pry"
-
 class CashRegister
   attr_accessor :total, :discount
   
   #@@count = 0 
   @@last_total = 0 
-  @@total = 0 
-  
+  #@@titles_array = []
   
   
   def initialize(discount = nil)
     @total = total
     @discount = discount
-    
     @total = 0
-    @@total = @total
-    
+    @@titles_array = []
     #@@count += 1
   end
   
-  
-  def add_item(title, price, quanitity = 1) 
-    @total = total
-    @@last_total << @total
-    self.total += price * quanitity
+  def add_item(title, price, quantity = 1) 
+    #@@last_total << @total
+    x = 0
+    if quantity >= 1 
+      while x < quantity
+      @@titles_array << title
+      x += 1 
+    end
+    @@last_total = price * quantity
+    self.total += price * quantity
   end
-  
+end
+
   def apply_discount(discount = @discount)
     @total = total
     if @discount == nil 
       "There is no discount to apply."
-      #binding.pry
     else 
       float_discount = discount / 100.0
       discounted_total = @total - (float_discount * @total)
@@ -38,5 +39,14 @@ class CashRegister
       "After the discount, the total comes to $#{discounted_total.to_int}."
     end
   end
+  
+  def items
+    @@titles_array
+  end
+  
+  def void_last_transaction
+    self.total = @total - @@last_total
+  end
 end
+
 
