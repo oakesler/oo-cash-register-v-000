b42527efb82cd63d3c61b47c961cfdaf3b8a7611
Olivia Kesler <oakesler@protonmail.ch>
2019-08-15 03:11:23 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,11 +4,39 @@ class CashRegister
   attr_accessor :total, :discount
   
   #@@count = 0 
+  @@last_total = 0 
+  @@total = 0 
   
   
-  def initialize(total = 0.0, discount = 0)
+  
+  def initialize(discount = nil)
     @total = total
     @discount = discount
+    
+    @total = 0
+    @@total = @total
+    
     #@@count += 1
   end
+  
+  
+  def add_item(title, price, quanitity = 1) 
+    @total = total
+    @@last_total << @total
+    self.total += price * quanitity
+  end
+  
+  def apply_discount(discount = @discount)
+    @total = total
+    if @discount == nil 
+      "There is no discount to apply."
+      #binding.pry
+    else 
+      float_discount = discount / 100.0
+      discounted_total = @total - (float_discount * @total)
+      @total = discounted_total
+      "After the discount, the total comes to $#{discounted_total.to_int}."
+    end
+  end
 end
+
